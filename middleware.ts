f1f5442f6964fc4925d5ab33ef428b1025355413
Joffre Veloz <joffreandres11@gmail.com>
2025-03-27 18:50:25 -0500

    feat: add /auth/sign-in and /auth/sign-up

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,12 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/forum(.*)"]);
+// const isPublicRoute = createRouteMatcher(["/auth/sign-in(.*)", "/auth/sign-up(.*), /"]);
 
 export default clerkMiddleware(async (auth, req) => {
+  // if (!isPublicRoute(req)) {
+  //   await auth.protect();
+  // }
   if (isProtectedRoute(req)) await auth.protect();
 });
 
